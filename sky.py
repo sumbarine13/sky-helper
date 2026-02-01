1fe37676d5ea78f8bc5dbded9bcfa3daf5c1bc43
sumbarine13 <sumbarine13@gmail.com>
2026-02-01 09:32:38 +0000

    Update sky.py

--- sky.py
+++ sky.py
@@ -37,5 +37,18 @@ async def sumbarine(ctx: commands.Context):
         "Sumbarine!"
     )
     await ctx.send(message)
+    from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return open("index.html").read()
+
+def run_web():
+    app.run(host="0.0.0.0", port=8080)
+
+Thread(target=run_web).start()
 
 bot.run(TOKEN)
